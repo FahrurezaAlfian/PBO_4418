c202ce139ac03c06b772c275cc6baec08236a48a
FahrurezaAlfian <111201710576@mhs.dinus.ac.id>
2021-03-16 18:53:38 +0700

    A11.2017.10576

---
+++ Latihan Tutrial Point/MyFirstJavaProgram.java
@@ -0,0 +1,14 @@
+//Nama 	: Fahrureza Alfian   
+//Kelas 	: A11.4418
+//NIM 	:  A11.2017.10576
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+   }
+}
+
